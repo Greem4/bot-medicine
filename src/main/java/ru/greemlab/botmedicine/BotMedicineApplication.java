4be177a2c97b42f3970e3171ef90c60c7ae1a13f
Greem4 <i@greem4.ru>
2025-02-08 18:00:16 +0300

    start project

---
+++ src/main/java/ru/greemlab/botmedicine/BotMedicineApplication.java
@@ -0,0 +1,13 @@
+package ru.greemlab.botmedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotMedicineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotMedicineApplication.class, args);
+    }
+
+}
